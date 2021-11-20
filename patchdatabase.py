c709336c74a2ed208d1b108f9c3c87cc58395376
Yann Girsberger <yann@ywg.ch>
2021-11-20 14:18:40 +0100

    [patchdatabase] Don't put PosixPath in patchdb.json

--- patchdatabase.py
+++ patchdatabase.py
@@ -33,7 +33,7 @@ class PatchDB:
         for rev in revs:
             commits.append(repo.rev_to_commit(rev))
         # To not be computer dependend, just work with the name of the patch
-        patch = Path(os.path.basename(patch))
+        patch = os.path.basename(patch)
         logging.debug(f"Saving entry for {patch}: {commits}")
 
         if patch not in self.data:
