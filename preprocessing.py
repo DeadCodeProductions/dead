03c51364e740fd841a6e168748dc81dda69a69a1
Yann Girsberger <42787138+Quarub@users.noreply.github.com>
2022-05-24 11:26:27 +0200

    Preprocessing fix (#19)
    [preprocessing] Change preprocessor to account for new compiler errors
    
    LLVM dropped a feature of C89 that automatically declares/creates a
    prototype for functions that have not been declared.
    Preprocssing did depend on that behaviour as it removed everything
    related to external function calls. It did so to ensure that preprocessed
    code is compatible across compilers.
    
    We remove some of declarations which have attributes other compilers
    don't know about. This works for CSmith code but might break down for 
    other projects.

--- preprocessing.py
+++ preprocessing.py
@@ -19,7 +19,7 @@ class PreprocessError(Exception):
 
 
 def find_marker_decl_range(lines: list[str], marker_prefix: str) -> tuple[int, int]:
-    p = re.compile(f"void {marker_prefix}(.*)\(void\);")
+    p = re.compile(rf"void {marker_prefix}(.*)\(void\);")
     first = 0
     for i, line in enumerate(lines):
         if p.match(line):
@@ -36,7 +36,7 @@ def find_marker_decl_range(lines: list[str], marker_prefix: str) -> tuple[int, i
 
 
 def find_platform_main_end(lines: Iterable[str]) -> Optional[int]:
-    p = re.compile(".*platform_main_end.*")
+    p = re.compile(r".*platform_main_end.*")
     for i, line in enumerate(lines):
         if p.match(line):
             return i
@@ -44,13 +44,59 @@ def find_platform_main_end(lines: Iterable[str]) -> Optional[int]:
 
 
 def remove_platform_main_begin(lines: Iterable[str]) -> list[str]:
-    p = re.compile(".*platform_main_begin.*")
+    p = re.compile(r".*platform_main_begin.*")
     return [line for line in lines if not p.match(line)]
 
 
 def remove_print_hash_value(lines: Iterable[str]) -> list[str]:
-    p = re.compile(".*print_hash_value = 1.*")
+    p = re.compile(r".*print_hash_value = 1.*")
     return [line for line in lines if not p.match(line)]
+
+
+def preprocess_lines(lines: list[str]) -> str:
+    start_patterns = [
+        re.compile(r"^extern.*"),
+        re.compile(r"^typedef.*"),
+        re.compile(r"^struct.*"),
+        # The following patterns are to catch if the last of the previous
+        # patterns in the file was tainted and we'd otherwise mark the rest
+        # of the file as tainted, as we'll find no end in this case.
+        re.compile(r"^static.*"),
+        re.compile(r"^void.*"),
+    ]
+    taint_patterns = [
+        re.compile(r".*__access__.*"),  # LLVM doesn't know about this
+        re.compile(r".*__malloc__.*"),
+        re.compile(
+            r".*_[F|f]loat[0-9]{1,3}x{0,1}.*"
+        ),  # https://gcc.gnu.org/onlinedocs/gcc/Floating-Types.html#Floating-Types
+        re.compile(r".*__asm__.*"),  # CompCert has problems
+    ]
+
+    def is_start(l: str) -> bool:
+        return any([p_start.match(l) for p_start in start_patterns])
+
+    lines_to_skip: list[int] = []
+    for i, line in enumerate(lines):
+        for p in taint_patterns:
+            if p.match(line):
+                # Searching for start of tainted region
+                up_i = i
+                up_line = lines[up_i]
+                while up_i > 0 and not is_start(up_line):
+                    up_i -= 1
+                    up_line = lines[up_i]
+
+                # Searching for end of tainted region
+                down_i = i + 1
+                down_line = lines[down_i]
+                while down_i < len(lines) and not is_start(down_line):
+                    down_i += 1
+                    down_line = lines[down_i]
+
+                lines_to_skip.extend(list(range(up_i, down_i)))
+
+    return "\n".join([line for i, line in enumerate(lines) if i not in lines_to_skip])
 
 
 def preprocess_csmith_file(
@@ -75,35 +121,8 @@ def preprocess_csmith_file(
             "-E",
         ] + additional_flags
         lines = utils.run_cmd(cmd).split("\n")
-        marker_range = find_marker_decl_range(lines, marker_prefix)
-        platform_main_end_line = find_platform_main_end(lines)
-        if not platform_main_end_line:
-            raise PreprocessError("Couldn't find 'platform_main_end'")
-        marker_decls = lines[marker_range[0] : marker_range[1]]
 
-        lines = lines[platform_main_end_line + 1 :]
-        lines = remove_print_hash_value(remove_platform_main_begin(lines))
-        lines = (
-            marker_decls
-            + [
-                "typedef unsigned int size_t;",
-                "typedef signed char int8_t;",
-                "typedef short int int16_t;",
-                "typedef int int32_t;",
-                "typedef long long int int64_t;",
-                "typedef unsigned char uint8_t;",
-                "typedef unsigned short int uint16_t;",
-                "typedef unsigned int uint32_t;",
-                "typedef unsigned long long int uint64_t;",
-                "int printf (const char *, ...);",
-                "void __assert_fail (const char *__assertion, const char *__file, unsigned int __line, const char *__function);",
-                "static void",
-                "platform_main_end(uint32_t crc, int flag)",
-            ]
-            + list(lines)
-        )
-
-        return "\n".join(lines)
+        return preprocess_lines(lines)
 
 
 def preprocess_csmith_code(
