9fbde17211e241f0fe2cae19842e32b6c9fa3de3
Theodoros Theodoridis <theodort@inf.ethz.ch>
2022-08-31 12:38:05 +0200

    Switch to a diopter based bisector

---
+++ dead/bisector.py
@@ -0,0 +1,94 @@
+from typing import Optional
+
+from diopter.bisector import Bisector, BisectionCallback
+from diopter.compiler import CompilationSetting, CompilerExe
+
+import ccbuilder
+
+from dead.checker import find_alive_markers
+from dead.utils import Case, DeadConfig
+
+
+# TODO: move this to diopter.CompilationSetting.with_commit
+def get_setting_with_commit(
+    commit: ccbuilder.Commit, setting: CompilationSetting, bldr: ccbuilder.Builder
+) -> CompilationSetting:
+    return CompilationSetting(
+        CompilerExe(
+            setting.compiler.project,
+            bldr.build(setting.compiler.project, commit, True),
+            commit,
+        ),
+        setting.opt_level,
+        setting.flags,
+        setting.include_paths,
+        setting.system_include_paths,
+    )
+
+
+class DeadBisectionCallback(BisectionCallback):
+    #TODO: this should operate directly on a case
+    def __init__(
+        self,
+        code: str,
+        setting: CompilationSetting,
+        marker: str,
+        bldr: ccbuilder.Builder,
+    ):
+        self.code = code
+        self.setting = setting
+        self.marker = marker
+        self.bldr = bldr
+
+    def check(self, commit: ccbuilder.Commit) -> Optional[bool]:
+        # try:
+        return self.marker in find_alive_markers(
+            self.code,
+            get_setting_with_commit(commit, self.setting, self.bldr),
+            "DCEMarker",
+        )
+        # except Exception as e:
+        # logging.warning(f"Test failed with: '{e}'. Continuing...")
+        return None
+
+
+def bisect_case(
+    case_: Case, bisector: Bisector, builder: ccbuilder.Builder, force: bool = False
+) -> bool:
+    if case_.bisection and not force:
+        return True
+    #TODO: drop this once we/if we switch to RegressionCase
+    def get_good_rev_for_bisection() -> ccbuilder.Revision:
+        for setting in case_.good_settings:
+            if (
+                setting.opt_level == case_.bad_setting.opt_level
+                and setting.compiler.project == case_.bad_setting.compiler.project
+            ):
+                return setting.compiler.revision
+        assert False, "bisect_case: this is not a regression"
+
+    callback = DeadBisectionCallback(
+        case_.code,
+        case_.bad_setting,
+        case_.marker,
+        builder,
+    )
+    assert callback.check(case_.bad_setting.compiler.revision)
+
+    #TODO: pass number of jobs and logfile
+    bisection_commit = bisector.bisect(
+        callback,
+        case_.bad_setting.compiler.revision,
+        get_good_rev_for_bisection(),
+        case_.bad_setting.compiler.project,
+        DeadConfig.get_config().gcc_repo
+        if case_.bad_setting.compiler.project == ccbuilder.CompilerProject.GCC
+        else DeadConfig.get_config().llvm_repo,  # there should be code somewhere for this
+    )
+
+    if not bisection_commit:
+        return False
+
+    case_.bisection = bisection_commit
+
+    return True
