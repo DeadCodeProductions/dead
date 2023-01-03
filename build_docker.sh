9cdf4a6e6cf839697e619a4d2795d69003cf1381
Yann Girsberger <yann@ywg.ch>
2023-01-03 19:49:23 +0100

    [docker] move from docker volume to directory

--- build_docker.sh
+++ build_docker.sh
@@ -1,39 +1,44 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 set -e
 
 echo "Building container tagged deaddocker. This will take some time..." 
 docker build . -t deaddocker
 
-echo "Creating docker volume named deadpersistent..." 
-docker volume create deadpersistent
+STORAGE=$(realpath ./docker_storage)
+
+if [[ ! -d $STORAGE ]];then 
+    mkdir $STORAGE
+fi
+
 
 echo "Preparing volume..."
+echo $STORAGE
 docker run -it \
-    -v deadpersistent:/persistent\
+    -v $STORAGE:/persistent\
     -v $(realpath ./patches/):/patches \
     deaddocker \
     sudo su -c "cp /patches/patchdb.json /persistent/patchdb.json &&\
-                (if [[ ! -d /persistent/logs ]]; then mkdir /persistent/logs; fi) &&\
-                (if [[ ! -d /persistent/compiler_cache ]]; then mkdir /persistent/compiler_cache; fi) &&\
+                (if [ ! -d /persistent/logs ]; then mkdir /persistent/logs; fi) &&\
+                (if [ ! -d /persistent/compiler_cache ]; then mkdir /persistent/compiler_cache; fi) &&\
                 chown dead:dead -R /persistent"
 docker run -it \
-    -v deadpersistent:/persistent\
+    -v $STORAGE:/persistent\
     deaddocker \
     sh -c "chmod 770 /persistent/compiler_cache &&\
            chmod g+rws /persistent/compiler_cache"
 
 docker run -it \
-    -v deadpersistent:/persistent\
+    -v $STORAGE:/persistent\
     deaddocker \
     sh -c "touch /persistent/casedb.sqlite3"
 
 docker run -it \
-    -v deadpersistent:/persistent\
+    -v $STORAGE:/persistent\
     deaddocker \
-    sh -c "cd /persistent && (if [[ ! -d gcc ]]; then git clone git://gcc.gnu.org/git/gcc.git; fi)"
+    sh -c "cd /persistent && (if [ ! -d gcc ]; then git clone git://gcc.gnu.org/git/gcc.git; fi)"
 
 docker run -it \
-    -v deadpersistent:/persistent\
+    -v $STORAGE:/persistent\
     deaddocker \
-    sh -c "cd /persistent && (if [[ ! -d llvm-project ]]; then git clone https://github.com/llvm/llvm-project; fi)"
+    sh -c "cd /persistent && (if [ ! -d llvm-project ]; then git clone https://github.com/llvm/llvm-project; fi)"
